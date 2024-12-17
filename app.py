7f00db04ecbaf506a9c9a76b93b534c398d57bae
Kate <kate@Kates-MBP.hitronhub.home>
2024-12-16 17:55:53 -0800

    Add the app file

---
+++ app.py
@@ -0,0 +1,12 @@
+# Write your code here
+# print('Card: ')
+# card = input()
+# print(card)
+# print('Definition: ')
+# definition = input()
+# print(definition)
+
+card = input('Card: ')
+print(card)
+definition = input('Definition: ')
+print(definition)
